0c161b76bd2efbee5e12b3983688c3ac4b4bc479
DForeman <dlydellf@hotmail.com>
2021-09-09 16:49:07 -0500

    Added prelim animation function (NOT g2g - still wip)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var id = null;
+function moveNotes() {
+    var notes = document.getElementById("musicalNotes");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 440) {
+            clearInterval(id)
+        } else {
+            pos++;
+            notes.style.top = pos + "px"; // to animate musicalNotes as "falling"
+        }
+    }
+}
