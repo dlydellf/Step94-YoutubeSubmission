27c8ba4a568798a542f415c43dd4b37ca678148c
DForeman <dlydellf@hotmail.com>
2021-09-09 22:24:24 -0500

    Further commented  the flow of/logic within nested function

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -9,14 +9,14 @@ function moveNotes() {
     var notes = document.getElementById("musicalNotes"); /* <obj> that will be animated */
     var pos = 100; /* sets animation's starting height */
     id = setInterval(frame, 10);
-    function frame() {
+    function frame() { /*--- see line 16's comment ---*/
         if (pos > 600) { /* once <obj> falls 600px from its starting height...*/
             notes.style.left = notes.style.left == "950px" ? "100px" : "950px"; /* ..it switches to the opposite side... */
             pos = 100; /*... and falls again*/
-        } else { /* ----- THIS 'ELSE' RUNS 1st --------*/
-            notes.style.opacity = 1; /* hidden <obj> appears, then...*/
+        } else { /*--- this function begins HERE, at this 'else' statement ---*/
+            notes.style.opacity = 1; /* transparent <obj> appears, then...*/
             pos++; /*...its position from top of parent container increases...*/
-            notes.style.top = pos + "px"; /*... as it animates ("falls") */
+            notes.style.top = pos + "px"; /*...as it animates (goto line 13) */
         }
     }
 }
